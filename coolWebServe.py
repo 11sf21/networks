020e3f2121c0a1baf2e115edcc97ea7955755e9a
11sf21 <11sf21@queensu.ca>
2015-07-16 10:47:38 -0400

    Create new file

---
+++ coolWebServe.py
@@ -0,0 +1,26 @@
+from socket import *
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 6789
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+while True:
+  print 'Ready to serve pages, dude'
+  connectionSocket, addr = serverSocket.accept()
+  try:
+    message = connectionSocket.recv(1024)
+    filename = message.split()[1]
+    f = open(filename[1:])
+    outputdata = f.read()
+    
+    httpMsg = 'HTTP/1.1 200 OK dude'
+    connectionSocket.send(httpMsg)
+    
+    for i in range(0, len(outputdata)):
+      connectionSocket.send(outputdata[i])
+    connectionSocket.close()
+  except IOError:
+    errorMsg = '404 Not found man, tough'
+    connectionSocket.send(errorMsg)
+    connectionSocket.close()
